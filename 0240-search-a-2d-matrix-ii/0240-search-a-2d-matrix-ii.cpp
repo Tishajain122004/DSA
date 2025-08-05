c3221a547b43dffc0c6f4002a52e6f515b84a21a
Tishajain122004 <147789887+Tishajain122004@users.noreply.github.com>
2025-08-05 17:41:15 +0530

    Time: 47 ms (82%), Space: 18.6 MB (67.5%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& mat, int target) {
-        int row = mat.size();
+
+        //brute force approach - 0(n**2)
+        // int row = mat.size();
         // int col = mat[0].size();
         // for(int i=0; i<row; i++){
         //     for(int j=0; j<col; j++){
@@ -11,24 +13,60 @@ public:
         //     }
         // }
         // return false;
-        int st = 0;
+
+        //more optimal - traversing all the rows and applied binary search for cols- 0(nlogm)
+        // int st = 0;
+        // int col = mat[0].size();
+        // for(int i=0; i<row; i++){
+        //     int st = 0;
+        //     int end = col - 1;
+        //     while(st<=end){
+        //         int mid = st + (end - st)/2;
+        //         if(mat[i][mid] == target){
+        //             return true;
+        //         }
+        //         else if(target>mat[i][mid]){
+        //             st = mid +1;
+        //         }
+        //         else{
+        //             end = mid -1;
+        //         }
+        //     }
+        // }
+        // return false;
+
+
+        //optimized solution - 
+        int row = mat.size();
         int col = mat[0].size();
-        for(int i=0; i<row; i++){
-            int st = 0;
-            int end = col - 1;
-            while(st<=end){
-                int mid = st + (end - st)/2;
-                if(mat[i][mid] == target){
-                    return true;
-                }
-                else if(target>mat[i][mid]){
-                    st = mid +1;
-                }
-                else{
-                    end = mid -1;
-                }
+        int st = col - 1;
+        int end = 0;
+
+        while(end<row && st>=0){
+            if(target == mat[end][st]){
+                return true;
+            }
+            else if(target>mat[end][st]){
+                end++;
+            }
+            else{
+                st--;
             }
         }
         return false;
+
+
+        // end = 0;
+        // while(end<=st){
+        //     if(target == mat[st][end]){
+        //         return true;
+        //     }
+        //     else if(target>mat[st][end]){
+        //         st--;
+        //     }
+        //     else{
+        //         end++;
+        //     }
+        // }
     }
 };
