15a446d2672538d55b6638de03cfd05466f85861
Tishajain122004 <147789887+Tishajain122004@users.noreply.github.com>
2025-08-08 11:07:31 +0530

    Time: 0 ms (100%), Space: 14.7 MB (50.4%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,14 +4,48 @@ public:
         vector<int> ans;
         int n = nums.size();
 
+        // for(int i=0; i<n; i++){
+        //     for(int j = i+1; j<n; j++){
+        //         if(nums[i] + nums[j] == target){
+        //             ans.push_back(i);
+        //             ans.push_back(j);
+        //             break;
+        //         }
+        //     }
+        // }
+
+        // using sorting 
+        // int small = 0;
+        // int large = n-1;
+        // sort(nums.begin(), nums.end());
+        // while(small <= large){
+        //     int sum = nums[small] + nums[large];
+        //     if(sum == target){
+        //         ans.push_back(small);
+        //         ans.push_back(large);
+        //         break;
+        //     }
+        //     else if(target < sum){
+        //         large--;
+        //     }
+        //     else{
+        //         small++;
+        //     }
+        // }
+        // return ans;
+
+        //using map - 
+        unordered_map<int, int> m;
         for(int i=0; i<n; i++){
-            for(int j = i+1; j<n; j++){
-                if(nums[i] + nums[j] == target){
-                    ans.push_back(i);
-                    ans.push_back(j);
-                    break;
-                }
-            }
+            int first = nums[i];
+            int second = target - first;
+            if(m.find(second) != m.end())   
+            {
+                ans.push_back(i);
+                ans.push_back(m[second]);
+                break;
+            } 
+            m[first] = i;
         }
         return ans;
     }
