a2d95766100be92196c1f200b86dbd34f7adf121
Tishajain122004 <147789887+Tishajain122004@users.noreply.github.com>
2025-09-02 14:22:50 +0530

    Time: 3 ms (34.98%), Space: 16.7 MB (10.91%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,41 +1,13 @@
-class Solution {
-public:
-    void sub(vector<int>& nums, vector<int>ans, int i, set<vector<int>>& getAllSub){
-
-        sort(nums.begin(), nums.end());
-
-        if(i == nums.size()){
-            getAllSub.insert(ans);
-            return;
-        }
-
-        //inclusion
-        ans.push_back(nums[i]);
-        sub(nums, ans, i+1, getAllSub);
-
-        ans.pop_back();
-        //exclusion
-        sub(nums, ans, i+1, getAllSub);
-         
-    }
-
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        int i =0; 
-        set<vector<int>>getAllSub;
-        vector<int> ans;
-        sub(nums, ans, i, getAllSub);
-
-        vector<vector<int>> result(getAllSub.begin(), getAllSub.end());
-        return result; 
-    }
-};
-
+//tisha 
 
 // class Solution {
 // public:
-//     void sub(vector<int>& nums, vector<int>ans, int i, vector<vector<int>>& getAllSub){
+//     void sub(vector<int>& nums, vector<int>ans, int i, set<vector<int>>& getAllSub){
+
+//         sort(nums.begin(), nums.end());
+
 //         if(i == nums.size()){
-//             getAllSub.push_back({ans});
+//             getAllSub.insert(ans);
 //             return;
 //         }
 
@@ -49,14 +21,50 @@ public:
          
 //     }
 
-//     vector<vector<int>> subsets(vector<int>& nums) {
+//     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
 //         int i =0; 
-//         vector<vector<int>>getAllSub;
+//         set<vector<int>>getAllSub;
 //         vector<int> ans;
-//          sub(nums, ans, i, getAllSub);
+//         sub(nums, ans, i, getAllSub);
 
-//         return getAllSub;
-        
+//         vector<vector<int>> result(getAllSub.begin(), getAllSub.end());
+//         return result; 
 //     }
-
 // };
+
+
+//shraadha khapra 
+class Solution {
+public:
+    void sub(vector<int>& nums, vector<int>ans, int i, vector<vector<int>>& getAllSub){
+
+        sort(nums.begin(), nums.end());
+
+        if(i == nums.size()){
+            getAllSub.push_back({ans});
+            return;
+        }
+
+        //inclusion
+        ans.push_back(nums[i]);
+        sub(nums, ans, i+1, getAllSub);
+
+        ans.pop_back();
+        //exclusion
+        int idx = i +1;
+        while(idx <nums.size() && nums[idx] == nums[idx -1]) idx++;
+        sub(nums, ans, idx, getAllSub);
+         
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        int i =0; 
+        vector<vector<int>>getAllSub;
+        vector<int> ans;
+        sub(nums, ans, i, getAllSub);
+
+        return getAllSub; 
+    }
+};
+
+
