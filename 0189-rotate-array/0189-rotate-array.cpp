25b8518aa5bbbe56e5215cafeb992efee148f8a9
Tishajain122004 <147789887+Tishajain122004@users.noreply.github.com>
2026-01-23 10:40:46 +0530

    Time: 0 ms (100%), Space: 29.7 MB (42.61%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -6,9 +6,32 @@ public:
         if(k == 0){
             return; 
         }
-        reverse(nums.begin(), nums.end());
+        int s = 0, e = n-1;
+        while(s<e){
+            swap(nums[s], nums[e]);
+            s++;
+            e--;
+        }
 
-        reverse(nums.begin(), nums.begin() + k);
-        reverse(nums.begin() + k, nums.end()); 
+        s =0;
+        e = k -1;
+
+        while(s<e){
+            swap(nums[s], nums[e]);
+            s++;
+            e--;
+        }
+
+        s = k;
+        e = n-1;
+        while(s<e){
+            swap(nums[s], nums[e]);
+            s++;
+            e--;
+        } 
+        // reverse(nums.begin(), nums.end());
+
+        // reverse(nums.begin(), nums.begin() + k);
+        // reverse(nums.begin() + k, nums.end()); 
     }
 };
