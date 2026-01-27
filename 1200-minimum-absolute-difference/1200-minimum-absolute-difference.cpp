a005ca67b055cb3244b37f3dd06f37d7d9912c00
Tishajain122004 <147789887+Tishajain122004@users.noreply.github.com>
2026-01-27 20:48:30 +0530

    Time: 14 ms (81.27%), Space: 36.7 MB (31.7%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
@@ -4,22 +4,35 @@ public:
         vector<vector<int>> ans; 
         sort(nums.begin(), nums.end());
 
-        int diff;
+        int curr_diff;
         int min_diff = INT_MAX;
 
-        for(int i=0; i<nums.size()-1; i++){
-            diff = nums[i+1] - nums[i];
-            min_diff = min(diff, min_diff); 
-        }
-
-
-
         for(int i=0; i<nums.size() - 1; i++){
-            int other_diff = nums[i+1] - nums[i];
-            if(min_diff == other_diff){
+            curr_diff = nums[i+1] - nums[i]; 
+            if(min_diff > curr_diff){
+                min_diff = curr_diff;
+                ans.clear();
+                ans.push_back({nums[i], nums[i+1]});
+            }
+            else if(min_diff == curr_diff){
                 ans.push_back({nums[i], nums[i+1]});
             }
         }
         return ans; 
+
+        // for(int i=0; i<nums.size()-1; i++){
+        //     diff = nums[i+1] - nums[i];
+        //     min_diff = min(diff, min_diff); 
+        // }
+
+
+
+        // for(int i=0; i<nums.size() - 1; i++){
+        //     int other_diff = nums[i+1] - nums[i];
+        //     if(min_diff == other_diff){
+        //         ans.push_back({nums[i], nums[i+1]});
+        //     }
+        // }
+        // return ans; 
     }
 };
