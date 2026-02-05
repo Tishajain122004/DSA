e38dd3edbf21392fddead49783f628f4888d4294
Tishajain122004 <147789887+Tishajain122004@users.noreply.github.com>
2026-02-05 08:40:14 +0530

    Time: 8 ms (46.52%), Space: 25.6 MB (91.36%) - LeetHub

---
+++ 3379-transformed-array/3379-transformed-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(n, 0);
+        for(int i=0; i<n; i++){
+            if(nums[i]==0){
+                result[i] = nums[i];
+            }
+            else if(nums[i] > 0){
+                int steps = (i + nums[i]) % n; 
+                result[i] = nums[steps];
+            }
+            else{
+                int steps = (i + nums[i]%n + n) % n; 
+                result[i] = nums[steps];
+            }
+        }
+        return result;
+        
+    }
+};
