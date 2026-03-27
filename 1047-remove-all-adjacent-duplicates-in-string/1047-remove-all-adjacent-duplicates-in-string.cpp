0341cc9cf249f400252e9a5479868599f0442306
Tishajain122004 <147789887+Tishajain122004@users.noreply.github.com>
2026-03-27 22:25:18 +0530

    Time: 8 ms (40.01%), Space: 13.5 MB (89.89%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,17 +1,35 @@
 class Solution {
 public:
     string removeDuplicates(string s) {
-        int i=0; 
-        int n = s.length() - 1; 
-        while(i<n){
-            while(s[i] == s[i+1]){
-                // string part = s.substr(i, i+1); 
-                s.erase(i, 2); 
-                n = s.length()-1; 
-                i=0; 
+        // int i=0; 
+        // int n = s.length() - 1; 
+        // while(i<n){
+        //     while(s[i] == s[i+1]){
+        //         s.erase(i, 2); 
+        //         n = s.length()-1; 
+        //         i=0; 
+        //     }
+        //     i++;
+        // }
+        // return s; 
+
+        int n = s.length(); 
+        string ans = ""; 
+        for(int i=0; i<n; i++){
+            if(ans.empty()){
+                ans.push_back(s[i]);
             }
-            i++;
+            
+            // else if(ans.back() != s[i]){
+            //     ans.push_back(s[i]); 
+            // }
+            else if(ans.back() == s[i]){
+                ans.pop_back(); 
+            }
+            else{
+                ans.push_back(s[i]); 
+            }
         }
-        return s; 
+        return ans; 
     }
 };
