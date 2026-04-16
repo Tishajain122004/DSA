ea164af6bef2ac63d26747d8307984e11936cdce
Tishajain122004 <147789887+Tishajain122004@users.noreply.github.com>
2026-04-17 00:26:30 +0530

    Time: 51 ms (49.85%), Space: 90.8 MB (44.57%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -5,13 +5,23 @@ public:
         int n = nums.size(); 
 
         for(int i=0; i<n; i++){
-            mp[nums[i]]++; 
-        }
-        for(auto it : mp){
-            if(it.second >1){
+            if(mp.find(nums[i]) != mp.end()){
                 return true; 
             }
+            mp[nums[i]]++; 
         }
         return false; 
+        // unordered_map<int , int>mp; 
+        // int n = nums.size(); 
+
+        // for(int i=0; i<n; i++){
+        //     mp[nums[i]]++; 
+        // }
+        // for(auto it : mp){
+        //     if(it.second >1){
+        //         return true; 
+        //     }
+        // }
+        // return false; 
     }
 };
