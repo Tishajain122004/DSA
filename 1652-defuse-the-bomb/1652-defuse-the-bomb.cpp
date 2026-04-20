fe26a8a441a3f1e9592bb0d89fed4942260831f7
Tishajain122004 <147789887+Tishajain122004@users.noreply.github.com>
2026-04-20 16:14:03 +0530

    Time: 0 ms (100%), Space: 11.5 MB (83.11%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.cpp
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.cpp
@@ -5,27 +5,37 @@ public:
         vector<int> ans(n, 0); 
 
         if(k==0){
-            // for(int i=0; i<n; i++){
-            //     nums[i] = 0; 
-            // }
             return ans; 
-
         }
         else if(k>0){
-            
-            // vector<int> ans(n, 0); 
-            for(int i=0; i<n; i++){
-                int tmp_sum = 0; 
-                for(int step = 1; step<=k; step++){
-                    int prev_idx = (i + step + n) % n; 
-                    tmp_sum += nums[prev_idx]; 
-                }
-                ans[i] = tmp_sum; 
+            int win_sum = 0; 
+            for(int i=1; i<=k; i++){
+                win_sum += nums[i]; 
             }
-            return ans; 
+
+            for(int i=0; i<n; i++){
+                ans[i] = win_sum; 
+
+                int add_idx = (i + k + 1) % n; 
+                win_sum += nums[add_idx];
+
+                int rm_idx = (i + 1 ) % n; 
+                win_sum -= nums[rm_idx];
+
+            }
+            
+            // for(int i=0; i<n; i++){
+            //     int tmp_sum = 0; 
+            //     for(int step = 1; step<=k; step++){
+            //         int prev_idx = (i + step + n) % n; 
+            //         tmp_sum += nums[prev_idx]; 
+            //     }
+            //     ans[i] = tmp_sum; 
+            // }
+            // return ans; 
         }
         else{
-            // vector<int> ans(n, 0);
+
             int prev_idx; 
             for(int i =0; i<n; i++){
             
@@ -35,7 +45,7 @@ public:
                     tmp_sum += nums[prev_idx]; 
                     
                 }
-                ans[i] += tmp_sum; 
+                ans[i] = tmp_sum; 
             }
             return ans; 
         }
