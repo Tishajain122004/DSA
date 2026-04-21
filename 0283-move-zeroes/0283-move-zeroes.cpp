6363751077c030b997c21e559982c036ee0c4505
Tishajain122004 <147789887+Tishajain122004@users.noreply.github.com>
2026-04-21 20:13:50 +0530

    Time: 0 ms (100%), Space: 23.7 MB (81.83%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,19 +1,52 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n = nums.size();
-        vector<int>ans(n, 0);
-        int j=0;
-        for(int i=0; i<n; i++){
+
+        //optimized - inplace - two pointers 
+        int n = nums.size(); 
+        int i=0, j=0; 
+
+        while(i<n){
+            if(nums[i] == 0){
+                i++;
+            }
+            else{
+                swap(nums[i], nums[j]); 
+                i++, j++; 
+            }
+        }
+
+        
+
+        while(i<n && j<n){
+            if(nums[j]  == 0 && nums[i] != 0){
+                swap(nums[i], nums[j]); 
+                i++, j++;
+            }
+            else if(nums[i] == 0){
+                i++;
+            }
+            else{
+                j++; 
+            }
+        }
+
+
+
+        //brute force - used extra space 
+        // int n = nums.size();
+        // vector<int>ans(n, 0);
+        // int j=0;
+        // for(int i=0; i<n; i++){
           
-                if(nums[i] != 0){
-                    ans[j] = nums[i]; 
-                    j++; 
-                }
+        //         if(nums[i] != 0){
+        //             ans[j] = nums[i]; 
+        //             j++; 
+        //         }
             
-        }
-        for(int i=0; i<n; i++){
-            nums[i] = ans[i]; 
-        }
+        // }
+        // for(int i=0; i<n; i++){
+        //     nums[i] = ans[i]; 
+        // }
     }
 };
