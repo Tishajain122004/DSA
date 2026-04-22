60d269508c21b8543dfa68833bc04e3015aae3c2
Tishajain122004 <147789887+Tishajain122004@users.noreply.github.com>
2026-04-22 19:58:45 +0530

    Time: 0 ms (100%), Space: 7.6 MB (96.61%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,37 +1,57 @@
 class Solution {
 public:
-
-    
-    bool detectCycle(int sum, unordered_set<int> &s){
-        if(s.find(sum) != s.end()){
-            return false; 
-        }
-        else{
-            s.insert(sum); 
-            return true; 
-        }
-    }
-
-    bool checkHappy(int n, unordered_set<int> &s){
-        if(n==1){
-            return true; 
-        }
+    int getNext(int n){
         int sum = 0; 
         while(n > 0){
             int digit = n % 10; 
             sum += digit * digit; 
             n /= 10; 
         }
-        if(!detectCycle(sum, s)){
-            return false;
-        }
-        else{
-            return checkHappy(sum, s); 
-        }
+        return sum; 
     }
-
     bool isHappy(int n) {
-        unordered_set<int>s;
-        return checkHappy(n, s); 
+        int slow = n; 
+        int fast = getNext(n); 
+        while(fast != 1 && slow != fast){
+            slow = getNext(slow);
+            fast = getNext(getNext(fast)); 
+        }
+        if(fast == 1){
+            return true;
+        }
+        return false; 
     }
+    
+    // bool detectCycle(int sum, unordered_set<int> &s){
+    //     if(s.find(sum) != s.end()){
+    //         return false; 
+    //     }
+    //     else{
+    //         s.insert(sum); 
+    //         return true; 
+    //     }
+    // }
+
+    // bool checkHappy(int n, unordered_set<int> &s){
+    //     if(n==1){
+    //         return true; 
+    //     }
+    //     int sum = 0; 
+    //     while(n > 0){
+    //         int digit = n % 10; 
+    //         sum += digit * digit; 
+    //         n /= 10; 
+    //     }
+    //     if(!detectCycle(sum, s)){
+    //         return false;
+    //     }
+    //     else{
+    //         return checkHappy(sum, s); 
+    //     }
+    // }
+
+    // bool isHappy(int n) {
+    //     unordered_set<int>s;
+    //     return checkHappy(n, s); 
+    // }
 };
