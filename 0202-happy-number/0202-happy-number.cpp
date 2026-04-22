d50f5dce21c3571a9a634e73dd687485a8004d3b
Tishajain122004 <147789887+Tishajain122004@users.noreply.github.com>
2026-04-22 19:44:08 +0530

    Time: 0 ms (100%), Space: 8.5 MB (5.8%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+
+    
+    bool detectCycle(int sum, unordered_set<int> &s){
+        if(s.find(sum) != s.end()){
+            return false; 
+        }
+        else{
+            s.insert(sum); 
+            return true; 
+        }
+    }
+
+    bool checkHappy(int n, unordered_set<int> &s){
+        if(n==1){
+            return true; 
+        }
+        int sum = 0; 
+        int pro = 1; 
+        while(n > 0){
+            int digit = n % 10; 
+            sum += digit * digit; 
+            n /= 10; 
+        }
+        if(!detectCycle(sum, s)){
+            return false;
+        }
+        else{
+            return checkHappy(sum, s); 
+        }
+    }
+
+    bool isHappy(int n) {
+        unordered_set<int>s;
+        return checkHappy(n, s);
+
+        
+
+    }
+};
