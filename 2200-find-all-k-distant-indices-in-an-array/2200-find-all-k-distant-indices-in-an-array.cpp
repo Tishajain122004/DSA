ba6cb0fd2fd372452cc97b0f6fc249a4202b8179
Tishajain122004 <147789887+Tishajain122004@users.noreply.github.com>
2026-04-28 22:27:49 +0530

    Time: 0 ms (100%), Space: 14.5 MB (90.46%) - LeetHub

--- 2200-find-all-k-distant-indices-in-an-array/2200-find-all-k-distant-indices-in-an-array.cpp
+++ 2200-find-all-k-distant-indices-in-an-array/2200-find-all-k-distant-indices-in-an-array.cpp
@@ -1,28 +1,53 @@
 class Solution {
 public:
     vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
+        int j=0; 
         int n = nums.size(); 
-        vector<int> new_arr;
-        vector<int> ans; 
+        vector<int>ans; 
 
-        for(int i=0; i<n; i++){
-            if(nums[i] == key){
-                new_arr.push_back(i);
-            }
-        }
+        while(j<n){
 
-        int i=0; 
-        while(i<n){
-            int j=0;
-            while(j<new_arr.size()){
-                if(abs(i-new_arr[j]) <= k){
-                    ans.push_back(i); 
-                    break;
+
+            if(nums[j] == key){
+                int st = max(j-k, 0); 
+                int end = min(j+k, n-1); 
+
+                if(!ans.empty() && ans.back() >= st){
+                    st = ans.back() +1 ; 
                 }
-                j++;
+                
+                for(int i=st; i<=end; i++){
+                    ans.push_back(i); 
+                }
+                
             }
-            i++;
+            j++;
         }
         return ans; 
+
+        //Brute Force - O(n*n)
+        // int n = nums.size(); 
+        // vector<int> new_arr;
+        // vector<int> ans; 
+
+        // for(int i=0; i<n; i++){
+        //     if(nums[i] == key){
+        //         new_arr.push_back(i);
+        //     }
+        // }
+
+        // int i=0; 
+        // while(i<n){
+        //     int j=0;
+        //     while(j<new_arr.size()){
+        //         if(abs(i-new_arr[j]) <= k){
+        //             ans.push_back(i); 
+        //             break;
+        //         }
+        //         j++;
+        //     }
+        //     i++;
+        // }
+        // return ans; 
     }
 };
