bb23c7248a8b6c411af3ce9354f43f750b990fc8
Tishajain122004 <147789887+Tishajain122004@users.noreply.github.com>
2026-04-29 13:52:25 +0530

    Time: 0 ms (100%), Space: 25.5 MB (97.45%) - LeetHub

--- 3194-minimum-average-of-smallest-and-largest-elements/3194-minimum-average-of-smallest-and-largest-elements.cpp
+++ 3194-minimum-average-of-smallest-and-largest-elements/3194-minimum-average-of-smallest-and-largest-elements.cpp
@@ -7,16 +7,18 @@ public:
         sort(nums.begin(), nums.end()); 
 
         int i=0, j=n-1;
+        double min_val = INT_MAX; 
         while(i<j){
             double val = (nums[i]+nums[j]) /2.0;
-            avgs.push_back(val);
+            min_val = min(min_val, val);
+
             i++, j--;
         }
 
-        double min_val = INT_MAX; 
-        for(int i=0; i<avgs.size(); i++){
-            min_val = min(min_val, avgs[i]);
-        }
+        // double min_val = INT_MAX; 
+        // for(int i=0; i<avgs.size(); i++){
+        //     min_val = min(min_val, avgs[i]);
+        // }
 
         return min_val; 
     }
