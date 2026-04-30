855726757ad9fee84129208d56521e26ee6eec88
Tishajain122004 <147789887+Tishajain122004@users.noreply.github.com>
2026-04-30 13:12:46 +0530

    Time: 0 ms (100%), Space: 9.9 MB (80.75%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,29 +1,54 @@
 class Solution {
 public:
-    void sub(vector<int>& nums, vector<int>ans, int i, vector<vector<int>>& getAllSub){
+    void f(vector<int> &nums, vector<int> &ans, vector<vector<int>> &allsub, int i){
         if(i == nums.size()){
-            getAllSub.push_back({ans});
-            return;
+            allsub.push_back({ans});
+            return; 
         }
 
-        //inclusion
-        ans.push_back(nums[i]);
-        sub(nums, ans, i+1, getAllSub);
-
-        ans.pop_back();
-        //exclusion
-        sub(nums, ans, i+1, getAllSub);
-         
+        ans.push_back(nums[i]); 
+        f(nums, ans, allsub, i+1); 
+        ans.pop_back(); 
+        f(nums, ans, allsub, i+1); 
     }
 
-    vector<vector<int>> subsets(vector<int>& nums) {
-        int i =0; 
-        vector<vector<int>>getAllSub;
-        vector<int> ans;
-         sub(nums, ans, i, getAllSub);
 
-        return getAllSub;
-        
+
+
+
+
+
+
+    // void sub(vector<int>& nums, vector<int>ans, int i, vector<vector<int>>& getAllSub){
+    //     if(i == nums.size()){
+    //         getAllSub.push_back({ans});
+    //         return;
+    //     }
+
+    //     //inclusion
+    //     ans.push_back(nums[i]);
+    //     sub(nums, ans, i+1, getAllSub);
+
+    //     ans.pop_back();
+    //     //exclusion
+    //     sub(nums, ans, i+1, getAllSub);
+         
+    // }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        // int i =0; 
+        // vector<vector<int>>getAllSub;
+        // vector<int> ans;
+        //  sub(nums, ans, i, getAllSub);
+
+        // return getAllSub;
+
+
+        int i=0; 
+        vector<int> ans; 
+        vector<vector<int>> allsub; 
+        f(nums, ans, allsub, i);
+        return allsub; 
     }
 
 };
