695d58e3828d742e1f597baf592e543e93f4a250
Tishajain122004 <147789887+Tishajain122004@users.noreply.github.com>
2026-05-05 11:50:11 +0530

    Time: 0 ms (100%), Space: 8.8 MB (50.95%) - LeetHub

--- 3794-reverse-string-prefix/3794-reverse-string-prefix.cpp
+++ 3794-reverse-string-prefix/3794-reverse-string-prefix.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     string reversePrefix(string s, int k) {
-        int n = s.length(); 
-        int i=1;
-        while(i<=n){
-            if(i==k){
-                reverse(s.begin(), s.begin() +k);
-                break; 
-            }
-            i++;
-        }
+
+        reverse(s.begin(), s.begin()+k); 
         return s; 
+        //using pointers
+        // int n = s.length(); 
+        // int i=1;
+        // while(i<=n){
+        //     if(i==k){
+        //         reverse(s.begin(), s.begin() +k);
+        //         break; 
+        //     }
+        //     i++;
+        // }
+        // return s; 
     }
 };
