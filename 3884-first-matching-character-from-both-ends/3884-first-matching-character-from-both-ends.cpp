4e3a39f6f3504c5176bb3466f31e7c53d4a58e01
Tishajain122004 <147789887+Tishajain122004@users.noreply.github.com>
2026-05-05 11:44:03 +0530

    Time: 0 ms (100%), Space: 8.9 MB (66.37%) - LeetHub

--- 3884-first-matching-character-from-both-ends/3884-first-matching-character-from-both-ends.cpp
+++ 3884-first-matching-character-from-both-ends/3884-first-matching-character-from-both-ends.cpp
@@ -1,23 +1,32 @@
 class Solution {
 public:
     int firstMatchingIndex(string s) {
-        int n = s.length(); 
-        if(n==1){
-            return 0;
-        }
-        int i = 0;
-        int j=n-1;
-        int ans = 0;
-        while(i<j){
-            if(s[i] == s[j]){
-                ans = i;
-                return ans; 
+        int n = s.length();
+        for(int i=0; i<n; i++){
+            if(s[i] == s[n-i-1]){
+                return i;
             }
-            i++, j--;
-        }
-        if(i==j){
-            return i;
         }
         return -1; 
+
+        //using 2 pointers
+        // int n = s.length(); 
+        // if(n==1){
+        //     return 0;
+        // }
+        // int i = 0;
+        // int j=n-1;
+        // int ans = 0;
+        // while(i<j){
+        //     if(s[i] == s[j]){
+        //         ans = i;
+        //         return ans; 
+        //     }
+        //     i++, j--;
+        // }
+        // if(i==j){
+        //     return i;
+        // }
+        // return -1; 
     }
 };
